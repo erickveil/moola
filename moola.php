545db7923c5fa3dc2cc17cc9ce530faa40f6bd03
Erick <erick@erickveil.com>
2013-04-07 08:00:19 -0700

    added moola.php, tha main viewing page

---
+++ moola.php
@@ -0,0 +1,34 @@
+<?php
+
+// moola.php
+//
+// Erick Veil
+// 2013-04-07
+// 
+// Personal Accounting Report page.
+//
+// Requires LAMP environment.
+//
+
+drawHTMLHead();
+drawCSS();
+drawHTMLBody();
+drawHTMLFoot();
+
+
+drawHTMLHead()
+{
+}
+
+drawCSS()
+{
+}
+
+drawHTMLBody()
+{
+}
+
+drawHTMLFoot()
+{
+}
+
