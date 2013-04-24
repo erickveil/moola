d9c451d1e5a6bb5bbae773ffd89194134ec1f79b
Erick Veil <registry@erickveil.com>
2013-04-23 19:13:07 -0700

    added datepicker to the ledger dates
    the onselect event of setting the datepicker will handle changing the
    value.

--- moola-control.js
+++ moola-control.js
@@ -7,18 +7,20 @@
 // javascript and jquery scripts to control various input modules in moola
 //
 
+// 0.1.0
 // after page loads
 $(function()
 {
     addDatepickers();
     
     $("[field=ledger_date]").change(function(){
-        var msg=$(this).attr("id");
-        alert(msg);
+        //var msg=$(this).attr("value");
+        //alert(msg);
     });
     
 });
 
+// 0.1.1
 // date picker attributes need to be added on each reload of a widget
 function addDatepickers()
 {   
@@ -34,6 +36,7 @@ function addDatepickers()
             $("#max").datepicker("option","minDate",selectedDate);
         }
     });
+
     $("#max").datepicker({
         defaultDate: def_max,    
         dateFormat: "yy-mm-dd",
@@ -42,8 +45,28 @@ function addDatepickers()
             $("#min").datepicker("option","maxDate",selectedDate);
         }
     });
+
+    $("[field=ledger_date]").datepicker({
+        defaultDate: $(this).attr("value"),    
+        dateFormat: "yy-mm-dd",
+        changeYear: false,
+        onClose: function(selectedDate){
+            ledgerDateChange(selectedDate);
+        }
+    });
 }
 
+// 0.1.1.1
+function ledgerDateChange(selectedDate)
+{
+    var val=$(this).attr("value");
+    if (selectedDate != val)
+        alert("mismatch");
+    else
+        alert("match");
+}
+
+// 0.2.0
 // pass the name of this function as a string to the php function,
 // "drawDateRange()" This establishes that the date range selector is a
 // sub-widget of the Ledger, and by selecting a range, and clicking the
